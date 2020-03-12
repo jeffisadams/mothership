331d419a9897130320a53049e735f5f0d3566a73
Jeffrey Adams <jeff@Jeffreys-MBP.fios-router.home>
2020-03-12 01:15:13 -0400

    Adding code for getTemp.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mothership
 Basic bash scripts to setup home automation and data collection.
+
+## Motivation
+This started because I was given a few Raspberry Pi Zeros and some temperature and Humidity gauges.  I thought it would be easy to write a bit of code and track temperature over time.  That part was pretty easy, and @sufinawaz had done most of the sensor work for me already.  All that was left was to install a database to track it.  How hard could it be to install elasticsearch?  Apparently harder than I thought.  So here are some scripts so I don't forget how.
+
+
+
+## Acknowledgements
+A lot of this was taken from here:
+- https://medium.com/hepsiburadatech/setup-elasticsearch-7-x-cluster-on-raspberry-pi-asus-tinker-board-6a307851c801
---
+++ getTemp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import Adafruit_DHT
+import requests
+import json
+import time
+
+# Parse command line parameters.
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+    print("Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4")
+    sys.exit(1)
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+temperature = temperature * 9/5.0 + 32 # In Farenheit
+
+# print("Temp is: ", temperature)
+# print("Humidity is: ", humidity)
+
+data = {'time': int(round(time.time() * 1000)),
+        'location': '{room of the house}',
+        'temp': temperature,
+        'humidity': humidity
+}
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+r = requests.post("http://{host}:9200/environment/_doc", data = json.dumps(data), headers = headers)
+
+print(r.text)
